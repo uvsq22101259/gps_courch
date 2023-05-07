cef8317c8540c9b900b27f169c9d1153e4beb944
uvsq22101259 <elie.kanga@ens.uvsq.fr>
2023-05-07 18:48:13 +0200

    finito

--- maintk.py
+++ maintk.py
@@ -1,28 +1,30 @@
 import tkinter as tk
-from tkinter import filedialog
-from PIL import ImageTk, Image
-from tkinter.messagebox import askquestion
-from tkinter.filedialog import askopenfilename
 import json
-import heapq
+from PIL import ImageTk, Image
 
-class Application(tk.Frame):
+
+
+class  Application(tk.Frame):
+    """ Classe principale de l'application"""
     def __init__(self, master=None):
         super().__init__(master)
         self.niveau = "débutant"
         self.root = root
         self.root.geometry("1500x1080")
         self.root.title("Createur de graph")
-        self.image_path = askopenfilename(defaultextension=".jpg", filetypes=[("JPEG files", "*.jpg")])
+        self.image_path = "data\plan-pistes.jpg"
         self.image = Image.open(self.image_path)
         self.photo = ImageTk.PhotoImage(self.image)
-        self.canvas = tk.Canvas(self.root, width=1400, height=1080, bg="black", scrollregion=(0, 0, self.image.width, self.image.height))
+        self.canvas = tk.Canvas(self.root, width=1400, height=1080, bg="black",
+                                scrollregion=(0, 0, self.image.width, self.image.height))
         self.canvas.grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.photo)
 
-        self.x_scrollbar = tk.Scrollbar(self.root, orient=tk.HORIZONTAL, command=self.canvas.xview, width= 40)
+        self.x_scrollbar = tk.Scrollbar(self.root, orient=tk.HORIZONTAL,
+                                        command=self.canvas.xview, width= 40)
         self.x_scrollbar.grid(row=1, column=0, sticky=tk.E+tk.W)
-        self.y_scrollbar = tk.Scrollbar(self.root, orient=tk.VERTICAL, command=self.canvas.yview, width= 40)
+        self.y_scrollbar = tk.Scrollbar(self.root, orient=tk.VERTICAL,
+                                        command=self.canvas.yview, width= 40)
         self.y_scrollbar.grid(row=0, column=1, sticky=tk.N+tk.S)
         self.canvas.config(xscrollcommand=self.x_scrollbar.set, yscrollcommand=self.y_scrollbar.set)
         menu_bar = tk.Menu(self.root)
@@ -30,8 +32,8 @@ class Application(tk.Frame):
         file_menu = tk.Menu(menu_bar, tearoff=0)
         file_menu.add_command(label="débutant", command= self.debutant)
         file_menu.add_command(label="confirmer", command= self.confimer)
-        
-        
+
+
         menu_bar.add_cascade(label="niveau", menu=file_menu)
         menu_bar.add_command(label="Exit", command=self.root.quit)
         menu_bar.add_command(label="reset", command=self.reset)
@@ -47,7 +49,8 @@ class Application(tk.Frame):
         # Ajouter les noeuds sur le canvas
         for noeud in liste_noeuds:
             x, y = noeud.coord
-            noeud_obj = self.canvas.create_oval(x-10, y-10, x+10, y+10, fill="orange", outline="black", width=2, tags="noeud")
+            noeud_obj = self.canvas.create_oval(x-10, y-10, x+10, y+10, fill="orange",
+                                            outline="black", width=2, tags="noeud")
             self.noeuds.append(noeud)
             noeud.point = noeud_obj
 
@@ -57,27 +60,30 @@ class Application(tk.Frame):
             for i in range(len(piste.coords)-1):
                 xi, yi = piste.coords[i]
                 xi1, yi1 = piste.coords[i+1]
-                piste_obj.append(self.canvas.create_line(xi, yi, xi1, yi1, fill=piste.couleur, width=5, tags= piste.nom))
+                piste_obj.append(self.canvas.create_line(xi, yi, xi1, yi1,
+                                                fill =piste.couleur, width=5, tags =piste.nom))
             self.pistes.append(piste)
             piste.segment = piste_obj
 
         # Ajouter les chemins sur le canvas
         self.canvas.bind("<Button-1>", self.clic_droit)
-    
+
 
 
     def confimer(self):
+        """ change le niveau de difficulté des pistes et des noeuds"""
         data.niveau = "confirmé"
         vitesse = {"green": 55, "blue": 60, "red": 100, "black": 130, "grey" : 1}
         for piste in self.pistes:
             piste.longueur /= vitesse[piste.couleur]
 
     def debutant(self):
+        """ change le niveau de difficulté des pistes et des noeuds"""
         data.niveau = "débutant"
         vitesse = {"green": 50, "blue": 45, "red": 40, "black": 35 , "grey" : 1}
         for piste in self.pistes:
             piste.longueur /= vitesse[piste.couleur]
-        
+
 
     def find_noeud(self, x, y):
         """ retourne le noeud sur lequel on a cliqué, ou None si on n'a pas cliqué sur un noeud"""
@@ -89,9 +95,11 @@ class Application(tk.Frame):
 
 
     def clic_droit(self,event):
-        """ gère le clic droit de la souris: si on a cliqué sur un noeud, on l'ajoute à la liste des chemins, sinon on affiche un message d'erreur"""
+        """ gère le clic droit de la souris: si on a cliqué sur un noeud,
+        on l'ajoute à la liste des chemins, sinon on affiche un message d'erreur"""
         if len(self.chemins) % 2 == 1 or len(self.chemins) == 0:
-            x, y = event.x + self.image.width*self.x_scrollbar.get()[0], event.y + self.image.height*self.y_scrollbar.get()[0]
+            x, y =(event.x + self.image.width*self.x_scrollbar.get()[0],
+                    event.y + self.image.height*self.y_scrollbar.get()[0])
             noeud = self.find_noeud(x, y)
             if noeud is not None:
                 self.chemins.append(noeud)
@@ -100,17 +108,13 @@ class Application(tk.Frame):
         if len(self.chemins) % 2 == 0 and len(self.chemins) != 0:
             self.trajet()
 
+
     def trajet(self,):
         """ gère l'affichage du trajet entre deux noeuds"""
-    
-        a = self.chemins[0]
-        b = self.chemins[1]
-        self.dijkstra(a,b)
-        
-     
 
-
-
+        debut = self.chemins[0]
+        fin = self.chemins[1]
+        self.dijkstra(debut,fin)
 
 
     def dijkstra(self, depart, arrivee):
@@ -120,11 +124,11 @@ class Application(tk.Frame):
         # Initialisation
         if depart == arrivee:
             return "vous êtes déjà sur place"
-        
+
         for noeud in self.noeuds:
             noeud.distance = float("inf")
             noeud.precedent = None
-        
+
         for vois in depart.voisins:
             longueur = data.get_piste(depart, vois).longueur
             vois.distance = longueur
@@ -132,7 +136,7 @@ class Application(tk.Frame):
 
         depart.distance = 0
         file = []
-        
+
         # Boucle principale
         while True:
             self.noeuds.sort(key = lambda x: x.distance)
@@ -142,20 +146,18 @@ class Application(tk.Frame):
                 print("Arrivée", noeud.precedent)
                 break
             for vois in noeud.voisins:
-                    
-                    longueur = data.get_piste(noeud, vois).longueur 
-                    print(longueur)
-                    
-                    if noeud.distance + longueur < vois.distance:
-                        vois.distance = noeud.distance + longueur
-                        vois.precedent = noeud
-                        
+
+                longueur = data.get_piste(noeud, vois).longueur
+                print(longueur)
+
+                if noeud.distance + longueur < vois.distance:
+                    vois.distance = noeud.distance + longueur
+                    vois.precedent = noeud
 
         # Affichage du chemin
         noeud = arrivee
 
         while noeud.precedent is not None:
-            
             self.canvas.itemconfig(noeud.point, fill="yellow", outline="yellow", width=4)
             piste = data.get_piste(noeud.precedent, noeud)
             for trait in piste.segment:
@@ -168,24 +170,21 @@ class Application(tk.Frame):
         self.chemins = []
 
     def reset(self):
+        """Remet le canvas à son état initial"""
         for noeud in self.noeuds:
             self.canvas.itemconfig(noeud.point, fill="orange", outline="black", width=1)
         for piste in self.pistes:
             for trait in piste.segment:
                 self.canvas.itemconfig(trait, fill=piste.couleur, width=5)
         self.chemins = []
-        
 
-        
-       
-        
 
-    
 
-file = json.load(open("data\data.json","r"))
+fichier = json.load(open("data\data.json","r"))
 
 
 class Noeuds ():
+    """Classe qui représente un noeud du graphe"""
     def __init__(self, nom,coord, point = None ) -> None:
         self.nom = nom
         self.voisins = list()
@@ -196,6 +195,7 @@ class Noeuds ():
         return self.nom
 
 class Pistes():
+    """Classe qui représente une piste de ski"""
     def __init__(self,nom, couleur, noeud_d, noeud_f, longueur, coords, segment = None) -> None:
         vitesse = {"green": 50, "blue": 45, "red": 40, "black": 35 , "grey" : 1}
         self.nom = nom
@@ -207,49 +207,56 @@ class Pistes():
         self.segment = segment
     def __repr__(self) -> str:
         return self.nom
-    
+
+
 class Data():
+    """Classe qui représente l'ensemble des données du graphe"""
     def __init__(self, noeuds, pistes) -> None:
         self.noeuds = noeuds
         self.pistes = pistes
+        self.niveau = "débutant"
         for piste in self.pistes:
             piste.depart = self.get_noeud(piste.depart)
             piste.fin = self.get_noeud(piste.fin)
         for noeud in self.noeuds:
             self.voisin(noeud)
 
-    
+
     def get_noeud(self, nom):
+        """Renvoie le noeud correspondant au nom donné"""
         for noeud in self.noeuds:
             if noeud.nom == nom:
                 return noeud
         return None
 
 
-    def get_piste(self, a, b):
+    def get_piste(self, debut, fin):
+        """Renvoie la piste la plus rapide entre les deux noeuds donnés"""
         pistes = []
         for piste in self.pistes:
-            if piste.depart == a and piste.fin == b:
+            if piste.depart == debut and piste.fin == fin:
                 pistes.append(piste)
         pistes.sort(key = lambda x: x.longueur)
         return pistes[0]
-    
+
+
     def voisin(self, noeud):
+        """Ajoute les voisins d'un noeud"""
         for piste in self.pistes:
             if piste.depart == noeud:
                 noeud.voisins.append(piste.fin)
 
 
 liste_noeuds = []
-for elem in file["noeuds"]:
+for elem in fichier["noeuds"]:
     liste_noeuds.append(Noeuds(elem["name"],(elem["x"],elem["y"])))
 
 liste_pistes = []
-for elem in file["pistes"]:
-    liste_pistes.append(Pistes(elem["name"], elem["couleur"], elem["noeud_depart"], elem["noeud_fin"], elem["longueur"], coords = elem["coords"]))
-    
-data = Data(liste_noeuds, liste_pistes)
+for elem in fichier["pistes"]:
+    liste_pistes.append(Pistes(elem["name"], elem["couleur"], elem["noeud_depart"],
+                               elem["noeud_fin"], elem["longueur"], coords = elem["coords"]))
 
+data = Data(liste_noeuds, liste_pistes)
 
 root = tk.Tk()
 app = Application(master=root)
