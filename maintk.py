90f32a243a5ad16d344390992dad974226cd61ca
uvsq22101259 <elie.kanga@ens.uvsq.fr>
2023-05-08 23:41:47 +0200

    suppr pop up chiant

--- maintk.py
+++ maintk.py
@@ -108,7 +108,7 @@ class  Application(tk.Frame):
             if noeud is not None:
                 self.chemins.append(noeud)
             else:
-                showinfo("Attention", "Vous n'avez pas cliqué sur un noeud")
+                pass
         if len(self.chemins) % 2 == 0 and len(self.chemins) != 0:
             self.trajet()
 
